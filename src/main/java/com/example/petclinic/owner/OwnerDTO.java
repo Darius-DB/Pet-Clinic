b3f8219ff174e2c873d451203fbcc5607b72010f
darius <dariusboengiu@gmail.com>
2022-10-24 16:12:23 +0300

    Added Dto for Pet and Owner

---
+++ src/main/java/com/example/petclinic/owner/OwnerDTO.java
@@ -0,0 +1,42 @@
+package com.example.petclinic.owner;
+
+import com.example.petclinic.pet.Pet;
+import lombok.*;
+
+import javax.validation.constraints.*;
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class OwnerDTO {
+
+    private Long ownerId;
+
+    @NotNull
+    private String firstName;
+
+    @NotNull
+    private String lastName;
+
+    @NotNull
+    private String city;
+
+    @NotNull  @Size(max = 10, min = 10) @Pattern(regexp = "^[0-9]*$")
+    private String phoneNumber;
+
+    @NotNull @Email
+    private String email;
+
+    private List<Pet> pets;
+
+    public OwnerDTO(String firstName, String lastName, String city, String phoneNumber, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.city = city;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+    }
+}
---
+++ src/main/java/com/example/petclinic/pet/PetDTO.java
@@ -0,0 +1,31 @@
+package com.example.petclinic.pet;
+
+import com.example.petclinic.owner.Owner;
+import lombok.*;
+
+import javax.validation.constraints.NotNull;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+public class PetDTO {
+
+    private Long petId;
+
+    @NotNull
+    private String species;
+    @NotNull
+    private Integer age;
+    @NotNull
+    private String name;
+
+    private Owner owner;
+
+    public PetDTO(String species, Integer age, String name) {
+        this.species = species;
+        this.age = age;
+        this.name = name;
+    }
+}
